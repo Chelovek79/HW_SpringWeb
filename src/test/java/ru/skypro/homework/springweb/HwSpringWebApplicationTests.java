28146abfbf359a6c66ebad3906513c2c78165b28
Vit <privit@rambler.ru>
2023-05-14 21:27:48 +0300

    в начале был код ...

---
+++ src/test/java/ru/skypro/homework/springweb/HwSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
